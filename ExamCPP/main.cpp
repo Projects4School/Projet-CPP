4ef1b611e5a2ce3652446d55714c006742a51f58
GenoWeb <evilbrain.exe@gmail.com>
2023-06-15 13:58:53 +0200

    Initial Commit

---
+++ ExamCPP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ExamCPP
+//
+//  Created by Gaspar Musitelli on 15/06/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
