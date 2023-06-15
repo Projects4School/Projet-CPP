e1848d10f5ec80d03650c94f7943304119e64af8
GenoWeb <evilbrain.exe@gmail.com>
2023-06-15 16:42:04 +0200

    Add competition code

--- ExamCPP/main.cpp
+++ ExamCPP/main.cpp
@@ -11,6 +11,14 @@
 
 using namespace Boxe;
 
+void AfficheResultats(Combat combats[], size_t size)
+{
+    for (size_t i = 0; i < size; i++) {
+        Combat& combat = combats[i];
+        std::cout << "\nCombat : " << combat.GetNiveau() << "\nCoin bleu : " << combat.GetCoinBleu()->GetNom() << "\nCoin rouge : " << combat.GetCoinRouge()->GetNom() << "\nVainqueur : " << combat.GetVainqueur()->GetNom() << std::endl;
+    }
+}
+
 int main(int argc, const char * argv[]) {
 
     // Init Boxeur
@@ -27,6 +35,32 @@ int main(int argc, const char * argv[]) {
     // App
     combat_1.SetCoinBleu(&boxeur_1);
     combat_1.SetCoinRouge(boxeur_2);
+    
+    // Competition
+    Combat combats[3] = {
+        Combat("Comb_1_1/2"),
+        Combat("Comb_2_1/2"),
+        Combat("Comb_3_1/1")
+    };
+    
+    Boxeur boxeur_3 = Boxeur("Box_3", 76.5);
+    Boxeur boxeur_4 = Boxeur("Box_4", 77.3);
+    
+    combats[0].SetCoinBleu(&boxeur_1);
+    combats[0].SetCoinRouge(boxeur_2);
+    
+    combats[1].SetCoinBleu(&boxeur_3);
+    combats[1].SetCoinRouge(&boxeur_4);
+    
+    combats[0].DesignerVainqueur("bleu");
+    combats[1].DesignerVainqueur("rouge");
+    
+    combats[2].SetCoinBleu(combats[0].GetVainqueur());
+    combats[2].SetCoinRouge(combats[1].GetVainqueur());
+    
+    combats[2].DesignerVainqueur("bleu");
+    
+    AfficheResultats(combats, 3);
     
     // Deinit
     delete boxeur_2;
