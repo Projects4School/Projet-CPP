672db7f14975e2a92d81a1c410785483a8e93c5d
GenoWeb <evilbrain.exe@gmail.com>
2023-06-15 15:04:41 +0200

    Add association tests

--- ExamCPP/main.cpp
+++ ExamCPP/main.cpp
@@ -13,18 +13,23 @@ using namespace Boxe;
 
 int main(int argc, const char * argv[]) {
 
-    // Boxeur tests
+    // Init Boxeur
     Boxeur boxeur_1 = Boxeur("Box_1", 75);
     std::cout << "Adresse : " << &boxeur_1 << " Nom : " << boxeur_1.GetNom() << " Poids : " << boxeur_1.GetPoids() << std::endl;
     
     Boxeur* boxeur_2 = new Boxeur("Box_2", 78);
     std::cout << "Adresse : " << boxeur_2 << " Nom : " << boxeur_2->GetNom() << " Poids : " << boxeur_2->GetPoids() << std::endl;
     
-    delete boxeur_2;
-    
-    // Combat tests
+    // Init combat
     Combat combat_1 = Combat("Comb_1_1/8");
     std::cout << "Adresse : " << &combat_1 << " Nom : " << combat_1.GetNiveau() << std::endl;
+    
+    // App
+    combat_1.SetCoinBleu(&boxeur_1);
+    combat_1.SetCoinRouge(boxeur_2);
+    
+    // Deinit
+    delete boxeur_2;
     
     return 0;
 }
